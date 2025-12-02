b09284aedaaf1b68c7f0151d891f0b64a105e942
RafaOrtiz94 <tskortiz@gmail.com>
2025-12-02 20:08:48 +0100

    Add GPS-enabled client visit tracking

--- spi_front/src/core/api/clientsApi.js
+++ spi_front/src/core/api/clientsApi.js
@@ -14,3 +14,13 @@ export const updateVisitStatus = async (clientId, payload) => {
   const { data } = await api.post(`/clients/${clientId}/visit-status`, payload);
   return data.data || data;
 };
+
+export const startClientVisit = async (clientId, payload = {}) => {
+  const { data } = await api.post(`/clients/${clientId}/visits/start`, payload);
+  return data.data || data;
+};
+
+export const endClientVisit = async (clientId, payload = {}) => {
+  const { data } = await api.post(`/clients/${clientId}/visits/end`, payload);
+  return data.data || data;
+};
